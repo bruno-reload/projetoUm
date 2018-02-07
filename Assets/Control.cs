dd024a98fdb5f6448cf8c44b93c26dd8f1c64f58
bruno_arabica <bruno-reload@live.com>
2018-02-07 19:43:57 -0200

    refataração rápida

--- Assets/Control.cs
+++ Assets/Control.cs
@@ -11,36 +11,25 @@ public class Control : MonoBehaviour
     public float gravity = 20.0F;
     public float jumpSpeed = 8.0F;
 
+    private float bordaEsquerda;
+    private float bordaDireita;
+    private float bordaSuperior;
+    private float bordaInferior;
+
+
+    private  float restricaoX;
+    private float restricaoZ;  
+
     void Start()
     {
 
     }
     void Update(){
-        move();
+        movimento();
     }
-    void move()
-    {
+    void movimento(){
+        limitAreaVisivel();
 
-        float distanciaZ = transform.position.z - Camera.main.transform.position.z;
-
-        float xPos = transform.position.x;
-
-        float bordaEsquerda = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanciaZ + xPos)).x;
-        float bordaDireita = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distanciaZ - xPos)).x;
-        float bordaSuperior = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanciaZ)).y;
-        float bordaInferior = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, distanciaZ)).y;
-
-        float restricaoX;
-        float restricaoZ; 
-
-#if UNITY_ANDROID
-        restricaoX = Mathf.Clamp(Touch.eixoX, bordaEsquerda, bordaDireita);
-        restricaoZ = Mathf.Clamp(Touch.eixoZ, bordaSuperior, bordaInferior);
-#endif
-#if UNITY_EDITOR
-        restricaoX = Mathf.Clamp(Input.GetAxis("Horizontal"), bordaEsquerda, bordaDireita);
-        restricaoZ = Mathf.Clamp(Input.GetAxis("Vertical"), bordaSuperior, bordaInferior);
-#endif
         CharacterController controller = GetComponent<CharacterController>();
 
         if (controller.isGrounded){
@@ -55,6 +44,29 @@ public class Control : MonoBehaviour
         } 
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+
+    }
+
+
+    public void limitAreaVisivel(){
+        
+        float distanciaZ = transform.position.z - Camera.main.transform.position.z;
+
+        float xPos = transform.position.x;
+
+        bordaEsquerda = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanciaZ + xPos)).x;
+        bordaDireita = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distanciaZ - xPos)).x;
+        bordaSuperior = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanciaZ)).y;
+        bordaInferior = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, distanciaZ)).y;
+
+#if UNITY_ANDROID
+        restricaoX = Mathf.Clamp(Touch.eixoX, bordaEsquerda, bordaDireita);
+        restricaoZ = Mathf.Clamp(Touch.eixoZ, bordaSuperior, bordaInferior);
+#endif
+#if UNITY_EDITOR
+        restricaoX = Mathf.Clamp(Input.GetAxis("Horizontal"), bordaEsquerda, bordaDireita);
+        restricaoZ = Mathf.Clamp(Input.GetAxis("Vertical"), bordaSuperior, bordaInferior);
+#endif
 
     }
 }
