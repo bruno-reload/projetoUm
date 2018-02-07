5f01bf40d56afc4413b8041c63f461edc8ec758c
bruno_arabica <bruno-reload@live.com>
2018-02-07 12:09:28 -0200

    base do projeto

---
+++ Assets/Control.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    public float speed = 10;
+    public float gravity = 20.0F;
+    public float jumpSpeed = 8.0F;
+
+    void Start()
+    {
+
+    }
+    void Update(){
+        move();
+    }
+    void move(){
+        
+        float distanciaZ = transform.position.z - Camera.main.transform.position.z;
+        float bordaEsquerda = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanciaZ + transform.position.x)).x;
+        float bordaDireita = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distanciaZ - transform.position.x)).x;
+        float bordaSuperior = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanciaZ)).y;
+        float bordaInferior = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, distanciaZ)).y;
+
+
+        CharacterController controller = GetComponent<CharacterController>();
+
+        float restricaoX = Mathf.Clamp(Touch.eixoX, bordaEsquerda, bordaDireita);
+        float restricaoZ = Mathf.Clamp(Touch.eixoZ, bordaSuperior, bordaInferior);
+
+        if (controller.isGrounded){
+            moveDirection = new Vector3(restricaoX, transform.position.y, restricaoZ);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Touch.jump)
+                moveDirection.y =  jumpSpeed;
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
+ 
---
+++ Assets/Touch.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour {
+
+    public static float eixoX = 0;
+    public static float eixoZ = 0;
+    public static bool jump = false;
+
+    private float max = 1;
+    private float min = 0;
+
+    public void CimaLiga(){
+        eixoZ = max;
+    }
+    public void CimaDesliga(){
+        eixoZ = min;
+    }
+    public void BaixoLiga(){
+        eixoZ = -max;
+    }
+    public void BaixoDesliga(){
+        eixoZ = min;
+    }
+    public void EsquerdaLiga()
+    {
+        eixoX = -max;
+    }
+    public void EsquerdaDesliga()
+    {
+        eixoX = min;
+    }
+    public void DireitaLiga()
+    {
+        eixoX = max;
+    }
+    public void DireitaDesliga()
+    {
+        eixoX = min;
+    }
+    public void PulaLiga(){
+        jump = true;
+    }
+    public void PulaDesliga(){
+        jump = false;
+    }
+}
